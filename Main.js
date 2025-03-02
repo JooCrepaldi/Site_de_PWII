08f82688997d98772bf76df9d99e37000e3c1337
JooCrepaldi <joaopedrocrepaldi05@gmail.com>
2025-03-02 18:39:51 -0300

    Initial commit

---
+++ Main.js
@@ -0,0 +1,7 @@
+/*document.getElementById("botaoLogin").addEventListener("click", function() {
+    document.querySelector(".loginPage").style.display = "none";
+    document.querySelector(".firstPage").style.display = "block";
+})*/ //desativado para formulação da página 1
+
+document.querySelector(".loginPage").style.display = "none";
+document.querySelector(".firstPage").style.display = "block";
