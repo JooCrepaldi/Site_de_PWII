1fef9401d4bf87acb82faaa41192bcf43c26ae34
JooCrepaldi <joaopedrocrepaldi05@gmail.com>
2025-03-09 12:08:29 -0300

    commit

--- scripts/Main.js
+++ scripts/Main.js
@@ -1,5 +1,10 @@
 document.getElementById("botaoLogin").addEventListener("click", function() {
     document.querySelector(".loginPage").style.display = "none";
     document.querySelector(".home").style.display = "block";
-}) //desativado para formulação da página 1
+})
+
+document.getElementById("sobrenosButton").addEventListener("click", function(){
+    document.querySelector(".home").style.display = "none";
+    document.querySelector(".sobreNos").style.display = "block";
+})
 
