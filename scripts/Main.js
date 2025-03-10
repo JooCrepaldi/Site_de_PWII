4dd35590db733710fc491d564b656b0edd43b1fd
JooCrepaldi <joaopedrocrepaldi05@gmail.com>
2025-03-10 11:47:58 -0300

    abas funcionando

--- scripts/Main.js
+++ scripts/Main.js
@@ -1,10 +1,23 @@
 document.getElementById("botaoLogin").addEventListener("click", function() {
     document.querySelector(".loginPage").style.display = "none";
-    document.querySelector(".home").style.display = "block";
-})
+    document.getElementById("home").style.display = "block";
+});
 
-document.getElementById("sobrenosButton").addEventListener("click", function(){
-    document.querySelector(".home").style.display = "none";
-    document.querySelector(".sobreNos").style.display = "block";
-})
+function showTab(tabId) {
+    let tabs = document.querySelectorAll('.tab-content');
+    tabs.forEach(tab => {
+        tab.style.display = "none";
+    });
+    
+    document.getElementById(tabId).style.display = "block";
+}
 
+document.addEventListener("DOMContentLoaded", function() {
+
+    let tabs = document.querySelectorAll('.tab-content');
+    tabs.forEach(tab => {
+        tab.style.display = "none";
+    });
+    
+    document.querySelector(".loginPage").style.display = "flex";
+});
